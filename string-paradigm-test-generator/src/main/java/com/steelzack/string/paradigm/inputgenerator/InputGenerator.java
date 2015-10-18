df15aac56679bf526afe12c5763d0a1f5de61b3a
Joao Esperancinha MH <jofisaes@hotmail.com>
2015-10-18 15:00:29 +0200

    SZ creates the getFullFileList method

--- string-paradigm-test-generator/src/main/java/com/steelzack/string/paradigm/inputgenerator/InputGenerator.java
+++ string-paradigm-test-generator/src/main/java/com/steelzack/string/paradigm/inputgenerator/InputGenerator.java
@@ -1,6 +1,12 @@
 package com.steelzack.string.paradigm.inputgenerator;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.kohsuke.args4j.CmdLineException;
@@ -13,5 +19,17 @@ public class InputGenerator {
 		final InputGeneratorOptions options = new InputGeneratorOptions();
 		final CmdLineParser parser = new CmdLineParser(options);
 		parser.parseArgument(args);
+	}
+
+	List<String> getFullFileList(InputStream io) throws IOException {
+		List<String> list = new ArrayList<String>();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(io, Charset.defaultCharset()))) {
+			String testRead = br.readLine();
+			while (testRead != null) {
+				list.add(testRead);
+				testRead = br.readLine();
+			}
+		}
+		return list;
 	}
 }
--- string-paradigm-test-generator/src/test/java/com/steelzack/string/paradigm/inputgenerator/InputGeneratorTest.java
+++ string-paradigm-test-generator/src/test/java/com/steelzack/string/paradigm/inputgenerator/InputGeneratorTest.java
@@ -1,8 +1,12 @@
 package com.steelzack.string.paradigm.inputgenerator;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -17,5 +21,49 @@ public class InputGeneratorTest {
 		final int retVal = pr.waitFor();
 
 		assertEquals(0, retVal);
+	}
+
+	@Test
+	public void testGetFullFileList() throws Exception {
+		final InputStream streamId = getClass().getResourceAsStream("sptg_id.txt");
+		final InputStream streamActor = getClass().getResourceAsStream("sptg_actor.txt");
+		final InputStream streamActress = getClass().getResourceAsStream("sptg_actress.txt");
+		final InputStream streamDirector = getClass().getResourceAsStream("sptg_director.txt");
+		final InputStream streamGenre = getClass().getResourceAsStream("sptg_genre.txt");
+		final InputStream streamLocation = getClass().getResourceAsStream("sptg_location.txt");
+		final InputStream streamMovie = getClass().getResourceAsStream("sptg_movie.txt");
+		final InputStream streamProducer = getClass().getResourceAsStream("sptg_producer.txt");
+		final InputStream streamSong = getClass().getResourceAsStream("sptg_song.txt");
+		final InputStream streamStuntMan = getClass().getResourceAsStream("sptg_stunt_man.txt");
+		final InputStream streamStuntWoman = getClass().getResourceAsStream("sptg_stunt_woman.txt");
+		final InputStream streamYear = getClass().getResourceAsStream("sptg_year.txt");
+
+		final InputGenerator inputGenerator = new InputGenerator();
+
+		final List<String> resultId = inputGenerator.getFullFileList(streamId);
+		final List<String> resultActor = inputGenerator.getFullFileList(streamActor);
+		final List<String> resultActress = inputGenerator.getFullFileList(streamActress);
+		final List<String> resultDirector = inputGenerator.getFullFileList(streamDirector);
+		final List<String> resultGenre = inputGenerator.getFullFileList(streamGenre);
+		final List<String> resultLocation = inputGenerator.getFullFileList(streamLocation);
+		final List<String> resultMovie = inputGenerator.getFullFileList(streamMovie);
+		final List<String> resultProducer = inputGenerator.getFullFileList(streamProducer);
+		final List<String> resultSong = inputGenerator.getFullFileList(streamSong);
+		final List<String> resultStuntMan = inputGenerator.getFullFileList(streamStuntMan);
+		final List<String> resultStuntWoman = inputGenerator.getFullFileList(streamStuntWoman);
+		final List<String> resultYear = inputGenerator.getFullFileList(streamYear);
+
+		assertThat(resultId, hasSize(5));
+		assertThat(resultActor, hasSize(8));
+		assertThat(resultActress, hasSize(4));
+		assertThat(resultDirector, hasSize(5));
+		assertThat(resultGenre, hasSize(4));
+		assertThat(resultLocation, hasSize(12));
+		assertThat(resultMovie, hasSize(6));
+		assertThat(resultProducer, hasSize(6));
+		assertThat(resultSong, hasSize(7));
+		assertThat(resultStuntMan, hasSize(6));
+		assertThat(resultStuntWoman, hasSize(1));
+		assertThat(resultYear, hasSize(12));
 	}
 }
