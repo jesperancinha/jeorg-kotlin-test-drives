0441ceb3bb0577b12aeea09ee6d9b2c5dd03001e
Joao Esperancinha <jofisaes@hotmail.com>
2015-10-21 16:50:47 +0200

    SZ Initial Ivy setup

---
+++ test-drive-xml-main-runner/src/main/java/com/steelzack/test/drive/books/XmlTestRunner.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.steelzack.test.drive.books;
+
+/**
+ * @author joao
+ *
+ */
+public class XmlTestRunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
