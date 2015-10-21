d30deee8a2e6b4657754ff853ec11ea811aa3511
Joao Esperancinha <jofisaes@hotmail.com>
2015-10-21 15:08:29 +0200

    SZ finishes the SAX pattern

--- test-drive-xml-sax/src/mainjava/com/steelzack/test/drive/books/xml/SAXBooksParser.java
+++ test-drive-xml-sax/src/mainjava/com/steelzack/test/drive/books/xml/SAXBooksParser.java
@@ -1,25 +1,60 @@
 package com.steelzack.test.drive.books.xml;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class SAXBooksParser extends DefaultHandler {
+	private static final String AUTHOR = "author";
+
+	private static final String BOOK = "book";
+
+	private static final String GENRE = "genre";
+
+	private static final String PAGES = "pages";
+
+	private static final String NAME = "name";
+
+	private String lastValue = null;
+
+	private Book currentBook = null;
+
+	private List<Book> books = new ArrayList<>();
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-		// TODO Auto-generated method stub
 		super.startElement(uri, localName, qName, attributes);
+		if (localName == BOOK) {
+			currentBook = new Book();
+			currentBook.setTitle(attributes.getValue(NAME));
+		}
 	}
 
 	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException {
-		// TODO Auto-generated method stub
 		super.endElement(uri, localName, qName);
+		if (localName.equals(PAGES)) {
+			currentBook.setPages(Integer.parseInt(lastValue));
+		} else if (localName.equals(GENRE)) {
+			currentBook.setGenre(lastValue);
+		} else if (localName.equals(AUTHOR)) {
+			currentBook.setAuthor(lastValue);
+		} else if (localName.equals(BOOK)) {
+			books.add(currentBook);
+		}
 	}
 
 	@Override
 	public void characters(char[] ch, int start, int length) throws SAXException {
-		// TODO Auto-generated method stub
 		super.characters(ch, start, length);
+		lastValue = new String(Arrays.copyOfRange(ch, start, start + length));
 	}
 }
--- test-drive-xml-sax/src/mainjava/com/steelzack/test/drive/xml/XmlBookSAXParserManager.java
+++ test-drive-xml-sax/src/mainjava/com/steelzack/test/drive/xml/XmlBookSAXParserManager.java
@@ -2,47 +2,54 @@ package com.steelzack.test.drive.xml;
 
 import java.io.InputStream;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import com.steelzack.test.drive.books.xml.SAXBooksParser;
+
 public class XmlBookSAXParserManager extends XmlBookParserBuilder implements XmlBookParserManager {
+	private SAXBooksParser handler = null;
 
 	XmlBookSAXParserManager(InputStream ioStream) {
 		super(ioStream);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public String getBookTitle(int bookNumber) {
-		// TODO Auto-generated method stub
-		return null;
+		return handler.getBooks().get(bookNumber).getTitle();
 	}
 
 	@Override
 	public int getNumberOfPages(int bookNumber) {
-		// TODO Auto-generated method stub
-		return 0;
+		return handler.getBooks().get(bookNumber).getPages();
 	}
 
 	@Override
 	public String getGenre(int bookNumber) {
-		// TODO Auto-generated method stub
-		return null;
+		return handler.getBooks().get(bookNumber).getGenre();
 	}
 
 	@Override
 	public String getAuthor(int bookNumber) {
-		// TODO Auto-generated method stub
-		return null;
+		return handler.getBooks().get(bookNumber).getAuthor();
 	}
 
 	@Override
 	public int getNumberOfBooks() {
-		// TODO Auto-generated method stub
-		return 0;
+		return handler.getBooks().size();
 	}
 
 	@Override
 	public void init() throws Exception {
-		// TODO Auto-generated method stub
-
+		SAXParserFactory spf = SAXParserFactory.newInstance();
+		spf.setNamespaceAware(true);
+		SAXParser saxParser = spf.newSAXParser();
+		XMLReader xmlReader = saxParser.getXMLReader();
+		handler = new SAXBooksParser();
+		xmlReader.setContentHandler(handler);
+		xmlReader.parse(new InputSource(getIoStream()));
 	}
-
 }
