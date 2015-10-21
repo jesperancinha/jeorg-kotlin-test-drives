c576921e391652c5b0e7c58b8d09509463ed6aec
Joao Esperancinha <jofisaes@hotmail.com>
2015-10-21 12:15:39 +0200

    SZ Sets up SAX environment

---
+++ test-drive-xml-sax/src/mainjava/com/steelzack/test/drive/books/xml/SAXBooksParser.java
@@ -0,0 +1,25 @@
+package com.steelzack.test.drive.books.xml;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXBooksParser extends DefaultHandler {
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		// TODO Auto-generated method stub
+		super.startElement(uri, localName, qName, attributes);
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		// TODO Auto-generated method stub
+		super.endElement(uri, localName, qName);
+	}
+
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		// TODO Auto-generated method stub
+		super.characters(ch, start, length);
+	}
+}
