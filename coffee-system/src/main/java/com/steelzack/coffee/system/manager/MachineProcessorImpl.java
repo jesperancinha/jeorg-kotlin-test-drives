68660ccea2db478ce24fb53d42db98fe03e7fec4
Joao <jofisaes@hotmail.com>
2016-04-30 16:09:15 +0200

    SZ-N Basic elements for MAchine and payment processor

--- coffee-system/src/main/java/com/steelzack/coffee/system/manager/MachineProcessorImpl.java
+++ coffee-system/src/main/java/com/steelzack/coffee/system/manager/MachineProcessorImpl.java
@@ -1,7 +1,15 @@
 package com.steelzack.coffee.system.manager;
 
+import lombok.Builder;
+import lombok.Getter;
+
 /**
  * Created by joaofilipesabinoesperancinha on 30-04-16.
  */
+@Builder
+@Getter
 public class MachineProcessorImpl implements MachineProcessor {
+    private final CoffeeProcessor coffeeProcessor;
+
+    private final PaymentProcessor paymentProcessor;
 }
--- coffee-system/src/main/java/com/steelzack/coffee/system/manager/PaymentProcessorImpl.java
+++ coffee-system/src/main/java/com/steelzack/coffee/system/manager/PaymentProcessorImpl.java
@@ -1,7 +1,13 @@
 package com.steelzack.coffee.system.manager;
 
+import lombok.Builder;
+import lombok.Getter;
+
 /**
  * Created by joaofilipesabinoesperancinha on 30-04-16.
  */
-public class PaymentProcessorImpl implements PaymentProcessor{
+@Builder
+@Getter
+public class PaymentProcessorImpl implements PaymentProcessor {
+Payment
 }
