748ac2ada7ccec43af9040f9aa4dab3666204e0c
Joao <jofisaes@hotmail.com>
2016-04-30 16:05:51 +0200

    SZ-N Adds Machine processor layer

---
+++ coffee-system/src/main/java/com/steelzack/coffee/system/launcher/CoffeeParadigmsMain.java
@@ -0,0 +1,10 @@
+package com.steelzack.coffee.system.launcher;
+
+/**
+ * Created by joaofilipesabinoesperancinha on 30-04-16.
+ */
+public class CoffeeParadigmsMain {
+    public static void main(String[] args) {
+
+    }
+}
