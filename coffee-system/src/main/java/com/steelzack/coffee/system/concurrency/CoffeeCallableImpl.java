9c8766f5a52b44a67b879b1acb021fb945d2adaf
Joao <jofisaes@hotmail.com>
2016-05-01 14:47:55 +0200

    SZ-N Completes Coffee Callable task

--- coffee-system/src/main/java/com/steelzack/coffee/system/concurrency/CoffeeCallableImpl.java
+++ coffee-system/src/main/java/com/steelzack/coffee/system/concurrency/CoffeeCallableImpl.java
@@ -1,16 +1,37 @@
 package com.steelzack.coffee.system.concurrency;
 
+import com.steelzack.coffee.system.input.CoffeeMachines.CoffeMachine.Coffees.Coffee.TimesToFill.FillTime;
+import com.sun.javafx.binding.StringFormatter;
 import lombok.Getter;
+import org.apache.log4j.Logger;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by joao on 29-4-16.
  */
 @Getter
 public class CoffeeCallableImpl implements Coffe, Callable<Boolean> {
+
+    final Logger logger = Logger.getLogger(CoffeeCallableImpl.class);
+
+    private FillTime fillTime;
+
+    public CoffeeCallableImpl(FillTime fillTime)
+    {
+        this.fillTime = fillTime;
+    }
+
     @Override
     public Boolean call() throws Exception {
+        logger.info(StringFormatter.format( //
+                "{0} - Starting tast {1} to make coffee", //
+                fillTime.getIndex(), //
+                fillTime.getDescription() //
+        ));
+        TimeUnit.MILLISECONDS.wait(fillTime.getValue());
         return true;
     }
+
 }
--- coffee-system/src/main/java/com/steelzack/coffee/system/concurrency/EmployeeCallableImpl.java
+++ coffee-system/src/main/java/com/steelzack/coffee/system/concurrency/EmployeeCallableImpl.java
@@ -1,8 +1,8 @@
 package com.steelzack.coffee.system.concurrency;
 
-import com.steelzack.coffee.system.input.CoffeeMachines;
 import com.steelzack.coffee.system.input.CoffeeMachines.CoffeMachine.Coffees.Coffee;
 import com.steelzack.coffee.system.input.CoffeeMachines.CoffeMachine.Coffees.Coffee.TimesToFill.FillTime;
+import com.steelzack.coffee.system.input.CoffeeMachines.CoffeMachine.PaymentTypes.Payment;
 import com.steelzack.coffee.system.input.Employees;
 import com.steelzack.coffee.system.input.Employees.Employee.Actions;
 import com.sun.javafx.binding.StringFormatter;
@@ -10,17 +10,21 @@ import lombok.Builder;
 import lombok.Getter;
 import org.apache.log4j.Logger;
 
+import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.Callable;
+import java.util.Set;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 @Builder
 @Getter
 public class EmployeeCallableImpl implements Employee, Callable<Boolean> {
-    final Logger logger = Logger.getLogger(EmployeeCallableImpl.class);
+    private final Logger logger = Logger.getLogger(EmployeeCallableImpl.class);
+
     private final Actions actions;
     private final Employees.Employee employee;
     private final Coffee chosenCoffee;
-    private final CoffeeMachines.CoffeMachine.PaymentTypes.Payment chosenPayment;
+    private final Payment chosenPayment;
 
     @Override
     public Boolean call() throws Exception {
@@ -36,15 +40,54 @@ public class EmployeeCallableImpl implements Employee, Callable<Boolean> {
 
     }
 
-    private void callMakeCoffee() {
+    private void callMakeCoffee() throws InterruptedException {
         final List<FillTime> tasks = chosenCoffee.getTimesToFill().getFillTime();
+        final Set<Integer> allIndexes = new HashSet<>();
+        final Set<Future<Boolean>> allResults = new HashSet<>();
         tasks.stream().sorted( //
-                (fillTime1, fillTime2)-> fillTime1.getIndex().compareTo(fillTime2.getIndex())
-        ).forEach( //
-                fillTime -> { //
-
-                }
+                (fillTime1, fillTime2) -> fillTime1.getIndex().compareTo(fillTime2.getIndex())
+        ).map(
+                FillTime::getIndex
+        ).forEach(
+                index -> allIndexes.add(index.intValue())
         );
+
+        for (Integer index : allIndexes) {
+            final List<FillTime> allTasksForIndex = tasks.stream().filter(
+                    fillTime -> fillTime.getIndex().intValue() == index
+            ).collect(Collectors.toList());
+            final ExecutorService executorService = Executors.newFixedThreadPool(allIndexes.size());
+            allTasksForIndex.stream().forEach(
+                    fillTime ->
+                    {
+                        CoffeeCallableImpl coffeeCallable = new CoffeeCallableImpl(fillTime);
+
+                        Future<Boolean> future = executorService.submit(coffeeCallable);
+                        allResults.add(future);
+                    }
+            );
+            allResults.stream().forEach(
+                    booleanFuture -> {
+                        try {
+                            if (!booleanFuture.get().booleanValue()) ;
+                            {
+                                logger.error("scheduled task faild to execute!");
+                            }
+                        } catch (InterruptedException e) {
+                            logger.error(e.getMessage(), e);
+                        } catch (ExecutionException e) {
+                            logger.error(e.getMessage(), e);
+                        }
+                    }
+            );
+        }
+
+
+//                .forEach( //
+//                fillTime -> { //
+//
+//                }
+//        );
     }
 
     private void callPostActions() {
