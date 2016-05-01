2fbb3127d599340df5ffe8d344365b05d5ea40fd
Joao <jofisaes@hotmail.com>
2016-05-01 09:17:15 +0200

    SZ-N Creates full skeleton for calls

--- coffee-system/src/main/java/com/steelzack/coffee/system/concurrency/EmployeeCallableImpl.java
+++ coffee-system/src/main/java/com/steelzack/coffee/system/concurrency/EmployeeCallableImpl.java
@@ -2,6 +2,7 @@ package com.steelzack.coffee.system.concurrency;
 
 import com.steelzack.coffee.system.input.CoffeeMachines;
 import com.steelzack.coffee.system.input.CoffeeMachines.CoffeMachine.Coffees.Coffee;
+import com.steelzack.coffee.system.input.CoffeeMachines.CoffeMachine.Coffees.Coffee.TimesToFill.FillTime;
 import com.steelzack.coffee.system.input.Employees;
 import com.steelzack.coffee.system.input.Employees.Employee.Actions;
 import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
@@ -29,11 +30,25 @@ public class EmployeeCallableImpl implements Employee, Callable<Boolean> {
     public Boolean call() throws Exception {
         logger.info(StringFormatter.format("Employee {0} is waiting in line", employee.getName()));
         callPreActions();
-
-
-
+        callMakeCoffee();
+        callPayCoffee();
         callPostActions();
         return true;
+    }
+
+    private void callPayCoffee() {
+
+    }
+
+    private void callMakeCoffee() {
+        final List<FillTime> tasks = chosenCoffee.getTimesToFill().getFillTime();
+        tasks.stream().sorted( //
+                (fillTime1, fillTime2)-> fillTime1.getIndex().compareTo(fillTime2.getIndex())
+        ).forEach( //
+                fillTime -> { //
+
+                }
+        );
     }
 
     private void callPostActions() {
