cea3fa8861ec8a662f48f22c62736c2bf8f742c2
Joao <jofisaes@hotmail.com>
2016-05-01 09:11:01 +0200

    SZ-N Adds chosen coffee and chosen payment type

--- coffee-system/src/main/java/com/steelzack/coffee/system/concurrency/EmployeeCallableImpl.java
+++ coffee-system/src/main/java/com/steelzack/coffee/system/concurrency/EmployeeCallableImpl.java
@@ -1,10 +1,13 @@
 package com.steelzack.coffee.system.concurrency;
 
+import com.steelzack.coffee.system.input.CoffeeMachines;
+import com.steelzack.coffee.system.input.CoffeeMachines.CoffeMachine.Coffees.Coffee;
 import com.steelzack.coffee.system.input.Employees;
 import com.steelzack.coffee.system.input.Employees.Employee.Actions;
 import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
 import com.sun.javafx.binding.StringFormatter;
 import com.sun.org.apache.xerces.internal.util.MessageFormatter;
+import lombok.Builder;
 import lombok.Getter;
 import org.apache.log4j.Logger;
 
@@ -13,32 +16,41 @@ import java.util.Formatter;
 import java.util.List;
 import java.util.concurrent.Callable;
 
+@Builder
 @Getter
 public class EmployeeCallableImpl implements Employee, Callable<Boolean> {
     final Logger logger = Logger.getLogger(EmployeeCallableImpl.class);
     private final Actions actions;
-    private Employees.Employee employee;
-
-    public EmployeeCallableImpl(Actions actions, Employees.Employee employee) {
-        this.actions = actions;
-        this.employee = employee;
-    }
+    private final Employees.Employee employee;
+    private final Coffee chosenCoffee;
+    private final CoffeeMachines.CoffeMachine.PaymentTypes.Payment chosenPayment;
 
     @Override
     public Boolean call() throws Exception {
         logger.info(StringFormatter.format("Employee {0} is waiting in line", employee.getName()));
-        final List<Actions.PreAction> preActions = this.actions.getPreAction();
-        preActions.stream().forEach( //
-                preAction -> { //
+        callPreActions();
 
-                } //
-        ); //
+
+
+        callPostActions();
+        return true;
+    }
+
+    private void callPostActions() {
         final List<Actions.PostAction> postActions = this.actions.getPostAction();
         postActions.stream().forEach( //
                 postAction -> { //
 
                 } //
         ); //
-        return true;
+    }
+
+    private void callPreActions() {
+        final List<Actions.PreAction> preActions = this.actions.getPreAction();
+        preActions.stream().forEach( //
+                preAction -> { //
+
+                } //
+        ); //
     }
 }
