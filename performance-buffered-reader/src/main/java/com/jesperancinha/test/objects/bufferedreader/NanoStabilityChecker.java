5e2f83048b22a7fabd1d81913072e217e5f73195
Joao Esperancinha <jofisaes@gmail.com>
2018-11-24 20:49:39 +0100

    JE-00019 Raises number of calls for more reliabale result

--- performance-buffered-reader/src/main/java/com/jesperancinha/test/objects/bufferedreader/NanoStabilityChecker.java
+++ performance-buffered-reader/src/main/java/com/jesperancinha/test/objects/bufferedreader/NanoStabilityChecker.java
@@ -1,19 +1,51 @@
 package com.jesperancinha.test.objects.bufferedreader;
 
 import java.io.PrintStream;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Used for testing the reliability of using nanoseconds in performance for unit
+ * The idea is that more calls will reduce the possibility of the stats to be wrong
  * cases.
- * 
- * @author JOAO
  *
+ * @author JOAO
  */
-public class NanoStabilityChecker {
+class NanoStabilityChecker {
 
-	public long printElementTo(String sentence, PrintStream out) {
+	private static Logger logger = LoggerFactory.getLogger(NanoStabilityChecker.class);
+
+	long printElementTo(String sentence, PrintStream out) {
 		long startMarker = System.nanoTime();
 		out.println(sentence);
 		return System.nanoTime() - startMarker;
+	}
+
+	/**
+	 * @param numberOfCalls
+	 * @param testString
+	 * @param checker
+	 * @return
+	 */
+	static Stats getStats(int numberOfCalls, String testString, NanoStabilityChecker checker) {
+		List<Integer> results = IntStream.range(0, numberOfCalls)
+				.map(index -> (int) checker.printElementTo(testString, System.out)).boxed().collect(
+						Collectors.toList());
+		long max = results.stream().max(Integer::compareTo).orElse(0);
+		long min = results.stream().min(Integer::compareTo).orElse(0);
+		long total = results.stream().reduce(Integer::sum).orElse(0);
+		final Stats stats = new Stats();
+		long average = total / numberOfCalls;
+		logger.info(String.format("Time for average check is %d ", average));
+		logger.info(String.format("Time for minimum check is %d ", min));
+		logger.info(String.format("Time for maximum check is %d ", max));
+		stats.setAverage(average);
+		stats.setMax(max);
+		stats.setMin(min);
+		return stats;
 	}
 }
--- performance-buffered-reader/src/test/java/com/jesperancinha/test/objects/bufferedreader/NanoStabilityCheckerTest.java
+++ performance-buffered-reader/src/test/java/com/jesperancinha/test/objects/bufferedreader/NanoStabilityCheckerTest.java
@@ -1,20 +1,19 @@
 package com.jesperancinha.test.objects.bufferedreader;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.logging.Logger;
+import static com.google.common.truth.Truth.assertThat;
+import static com.jesperancinha.test.objects.bufferedreader.NanoStabilityChecker.getStats;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * 
  * @author JOAO
- *
  */
 public class NanoStabilityCheckerTest {
-	private static final int NUMBER_OF_TEST_CALLS = 100000;
+	private static final int NUMBER_OF_TEST_CALLS = 1000000;
 
-	private static final Logger logger = Logger.getLogger(NanoStabilityChecker.class.getName());
+	private static final Logger logger = LoggerFactory.getLogger(NanoStabilityChecker.class);
 
 	@Test
 	public void testPrintElementTo_100000_elements() {
@@ -26,7 +25,7 @@ public class NanoStabilityCheckerTest {
 		for (int i = 1; i <= NUMBER_OF_TEST_CALLS; i++) {
 			final String testString = "Test string" + Math.random();
 			final long resultTimeForThisCheck = checker.printElementTo(testString, System.out);
-			logger.info(String.format("Time for check %d is %d", i, resultTimeForThisCheck));
+			logger.info("Time for check {} is {}", i, resultTimeForThisCheck);
 			testInToleranceRange(resultTimeForThisCheck, stats);
 		}
 	}
@@ -34,32 +33,9 @@ public class NanoStabilityCheckerTest {
 	private void testInToleranceRange(long resultTimeForThisCheck, Stats stats) {
 		long maxValue = stats.getMax();
 		long minValue = stats.getMin();
-		assertTrue(resultTimeForThisCheck >= minValue ? resultTimeForThisCheck <= maxValue : false);
+		assertThat(resultTimeForThisCheck).isAtLeast(minValue);
+		assertThat(resultTimeForThisCheck).isAtMost(maxValue);
 		logger.info(String.format("Test passed for check between %d and %d", minValue, maxValue));
 	}
 
-	private Stats getStats(int numberOfCalls, String testString, NanoStabilityChecker checker) {
-		long average = checker.printElementTo(testString, System.out);
-		long max = average;
-		long min = average;
-		for (int i = 0; i < numberOfCalls; i++) {
-			final long printElementToTime = checker.printElementTo(testString, System.out);
-			if (printElementToTime > max) {
-				max = printElementToTime;
-			}
-
-			if (printElementToTime < min) {
-				min = printElementToTime;
-			}
-			average = (average + printElementToTime) / 2;
-		}
-		final Stats stats = new Stats();
-		logger.info(String.format("Time for average check is %d ", average));
-		logger.info(String.format("Time for minimum check is %d ", min));
-		logger.info(String.format("Time for maximum check is %d ", max));
-		stats.setAverage(average);
-		stats.setMax(max);
-		stats.setMin(min);
-		return stats;
-	}
 }
