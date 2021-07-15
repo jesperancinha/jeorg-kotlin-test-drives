779e2699e239b7afddd02a5184b60c476a5eb0ef
Joao Esperancinha <jofisaes@gmail.com>
2021-07-15 15:06:27 +0200

    jeorg-kotlin-crums-2  - Crum 20

---
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-2/src/main/kotlin/org/jesperancinha/ktd/crums2/crum20/Cake.kt
@@ -0,0 +1,8 @@
+package org.jesperancinha.ktd.crums2.crum20
+
+class Cake {
+
+    open fun symbol() {
+        return
+    }
+}
---
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-2/src/main/kotlin/org/jesperancinha/ktd/crums2/crum20/CrumTwenty.kt
@@ -0,0 +1,10 @@
+package org.jesperancinha.ktd.crums2.crum20
+
+class CrumTwenty {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+        }
+    }
+}
