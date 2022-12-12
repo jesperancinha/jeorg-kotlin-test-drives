ed0f7725f45f8f0e95d029a7200d9e235b1d3600
Joao Esperancinha <jofisaes@gmail.com>
2022-12-12 14:38:37 +0100

    Open end ranges

--- jeorg-kotlin-crums/jeorg-kotlin-crums-3/src/main/kotlin/org/jesperancinha/ktd/crums3/Main.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-3/src/main/kotlin/org/jesperancinha/ktd/crums3/Main.kt
@@ -1,10 +1,12 @@
 package org.jesperancinha.ktd.crums3
 
 import org.jesperancinha.ktd.crums3.crum1.CrumOne
+import org.jesperancinha.ktd.crums3.crum2.CrumTwo
 
 fun main(args: Array<String>) {
     CrumOne()
 
     CrumOne.main(args)
+    CrumTwo.main(args)
 }
 
