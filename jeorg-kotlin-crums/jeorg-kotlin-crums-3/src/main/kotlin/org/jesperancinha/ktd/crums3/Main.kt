e917d6d031d8d1c2597975c22f7b9644cf5b7b23
Joao Esperancinha <jofisaes@gmail.com>
2022-12-12 19:47:30 +0100

    Arrow optics programmatically made

--- jeorg-kotlin-crums/jeorg-kotlin-crums-3/src/main/kotlin/org/jesperancinha/ktd/crums3/Main.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-3/src/main/kotlin/org/jesperancinha/ktd/crums3/Main.kt
@@ -4,6 +4,7 @@ import org.jesperancinha.ktd.crums3.crum1.CrumOne
 import org.jesperancinha.ktd.crums3.crum2.CrumTwo
 import org.jesperancinha.ktd.crums3.crum3.CrumThree
 import org.jesperancinha.ktd.crums3.crum4.CrumFour
+import org.jesperancinha.ktd.crums3.crum5.CrumFive
 
 fun main(args: Array<String>) {
     CrumOne()
@@ -12,5 +13,6 @@ fun main(args: Array<String>) {
     CrumTwo.main(args)
     CrumThree.main(args)
     CrumFour.main(args)
+    CrumFive.main(args)
 }
 
---
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-3/src/main/kotlin/org/jesperancinha/ktd/crums3/crum5/CrumFive.kt
@@ -0,0 +1,37 @@
+package org.jesperancinha.ktd.crums3.crum5
+
+import arrow.optics.optics
+import arrow.optics.*
+import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
+
+data class Account(val balance: Int, val available: Int) {
+    companion object
+}
+
+val accountLens: Lens<Account, Int> = Lens(
+    get = { account -> account.balance},
+    set = { player, value -> player.copy(balance = value, available = player.available+ value) }
+)
+
+val account= Account(70, 60)
+class CrumFive{
+
+    companion object{
+        private val logger = object {
+            fun info(logText: String) = ConsolerizerComposer.out().red(logText)
+
+            fun infoTitle(logText: String) = ConsolerizerComposer.outSpace()
+                .yellow(ConsolerizerComposer.title(logText))
+        }
+        @JvmStatic
+        fun main(args: Array<String>) {
+            logger.infoTitle("Crum 5 - Programmatically made optics in Arrow")
+
+            logger.info("Current account status: $account")
+            val result = accountLens.set(account, 10)
+            logger.info("Current account status: $result")
+            val result2 = accountLens.set(result, -50)
+            logger.info("Current account status: $result2")
+        }
+    }
+}
