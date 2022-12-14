85b65a0245fbdd33ebf8fd7629ee9232d71a356b
Joao Esperancinha <jofisaes@gmail.com>
2022-12-14 22:11:28 +0100

    A few generate prism exercises

--- jeorg-kotlin-crums/jeorg-kotlin-crums-4/src/main/kotlin/org/jesperancinha/ktd/crums3/crums3/CrumThree.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-4/src/main/kotlin/org/jesperancinha/ktd/crums3/crums3/CrumThree.kt
@@ -5,7 +5,7 @@ import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
 
 @optics
 sealed class House {
-    companion object{}
+    companion object {}
     data class Hut(val straws: Int) : House()
     data class Villa(val bricks: Int) : House()
     data class Apartment(val bricks: Int) : House()
@@ -13,13 +13,14 @@ sealed class House {
 
 class CrumThree {
 
-    companion object{
+    companion object {
         private val logger = object {
-            fun info(logText: Any) = ConsolerizerComposer.out().orange(logText)
+            fun info(logText: Any?) = ConsolerizerComposer.out().orange(logText)
 
             fun infoTitle(logText: String) = ConsolerizerComposer.outSpace()
                 .brightMagenta(ConsolerizerComposer.title(logText))
         }
+
         @JvmStatic
         fun main(args: Array<String> = emptyArray()) {
             logger.infoTitle("Crum 3 - Prisms with code generation")
@@ -27,6 +28,24 @@ class CrumThree {
             val hutPrism = House.hut
             val villaPrism = House.villa
             val apartment = House.apartment
+
+            val hut1 = House.Hut(1000)
+            val result1 = hutPrism.modify(hut1) {
+                House.Hut(it.straws * 9)
+            }
+            val result12 = hutPrism.lift {
+                House.Hut(it.straws * 8)
+            }
+            logger.info(hut1)
+            logger.info(result1)
+            logger.info(result12(hut1))
+            val orNull = hutPrism.getOrNull(House.Hut(500))
+            val orNull2 = hutPrism.getOrNull(House.Villa(500))
+            val orNull3 = hutPrism.getOrNull(House.Apartment(500))
+            logger.info(orNull)
+            logger.info(orNull2)
+            logger.info(orNull3)
         }
+
     }
 }
