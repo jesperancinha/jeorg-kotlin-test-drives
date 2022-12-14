ea1fa8e67390babea73cf5cd829b507dbf041acd
Joao Esperancinha <jofisaes@gmail.com>
2022-12-14 21:49:32 +0100

    Generate prisms

--- jeorg-kotlin-crums/jeorg-kotlin-crums-4/src/main/kotlin/org/jesperancinha/ktd/crums3/crums3/CrumThree.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-4/src/main/kotlin/org/jesperancinha/ktd/crums3/crums3/CrumThree.kt
@@ -1,7 +1,15 @@
 package org.jesperancinha.ktd.crums3.crums3
 
+import arrow.optics.optics
 import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
-import org.jesperancinha.ktd.crums3.crum2.CrumTwo
+
+@optics
+sealed class House {
+    companion object{}
+    data class Hut(val straws: Int) : House()
+    data class Villa(val bricks: Int) : House()
+    data class Apartment(val bricks: Int) : House()
+}
 
 class CrumThree {
 
@@ -16,6 +24,9 @@ class CrumThree {
         fun main(args: Array<String> = emptyArray()) {
             logger.infoTitle("Crum 3 - Prisms with code generation")
 
+            val hutPrism = House.hut
+            val villaPrism = House.villa
+            val apartment = House.apartment
         }
     }
 }
