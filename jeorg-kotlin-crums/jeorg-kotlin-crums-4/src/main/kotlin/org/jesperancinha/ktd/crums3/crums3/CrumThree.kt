c15e45e1530ed9a4876a869ea9175947b1fb12fa
Joao Esperancinha <jofisaes@gmail.com>
2022-12-15 08:38:09 +0100

    Exploring prism in its depths

--- jeorg-kotlin-crums/jeorg-kotlin-crums-4/src/main/kotlin/org/jesperancinha/ktd/crums3/crums3/CrumThree.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-4/src/main/kotlin/org/jesperancinha/ktd/crums3/crums3/CrumThree.kt
@@ -16,7 +16,8 @@ class CrumThree {
     companion object {
         private val logger = object {
             fun info(logText: Any?) = ConsolerizerComposer.out().orange(logText)
-
+            fun info2(logText: Any?) = ConsolerizerComposer.out().red(logText)
+            fun comment(logText: Any?) = ConsolerizerComposer.out().green(logText)
             fun infoTitle(logText: String) = ConsolerizerComposer.outSpace()
                 .brightMagenta(ConsolerizerComposer.title(logText))
         }
@@ -27,9 +28,12 @@ class CrumThree {
 
             val hutPrism = House.hut
             val villaPrism = House.villa
-            val apartment = House.apartment
+            val apartmentPrim = House.apartment
 
             val hut1 = House.Hut(1000)
+            val villa1 = House.Villa(2000)
+            val apartment1 = House.Apartment(3000)
+
             val result1 = hutPrism.modify(hut1) {
                 House.Hut(it.straws * 9)
             }
@@ -45,6 +49,33 @@ class CrumThree {
             logger.info(orNull)
             logger.info(orNull2)
             logger.info(orNull3)
+
+            val reverseGet = hutPrism.reverseGet(House.Hut(111))
+            val reverseGet2 = villaPrism.reverseGet(House.Villa(111))
+            val reverseGet3 = apartmentPrim.reverseGet(House.Apartment(111))
+
+            logger.info(reverseGet)
+            logger.info(reverseGet2)
+            logger.info(reverseGet3)
+
+            val liftHut = hutPrism.lift { it } (hut1)
+            val liftVilla = villaPrism.lift { it } (villa1)
+            val liftApartment = apartmentPrim.lift { it } (apartment1)
+
+            logger.info2(liftHut)
+            logger.info2(liftVilla)
+            logger.info2(liftApartment)
+
+            logger.comment("Compose works as an and and so everything is null")
+            val composeGroundHouses = hutPrism compose villaPrism
+            val orNullComposed1 = composeGroundHouses.getOrNull(hut1)
+            val orNullComposed2 = composeGroundHouses.getOrNull(villa1)
+            val orNullComposed3 = composeGroundHouses.getOrNull(apartment1)
+            logger.info(orNullComposed1)
+            logger.info(orNullComposed2)
+            logger.info(orNullComposed3)
+
+
         }
 
     }
