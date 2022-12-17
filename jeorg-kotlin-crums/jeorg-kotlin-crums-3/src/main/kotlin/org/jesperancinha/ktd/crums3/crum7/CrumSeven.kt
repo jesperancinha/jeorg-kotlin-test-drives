06ebbf617059a9cd3212b0e1b7cb8b4ac9461c7b
Joao Esperancinha <jofisaes@gmail.com>
2022-12-17 16:27:57 +0100

    Either checks

--- jeorg-kotlin-crums/jeorg-kotlin-crums-3/src/main/kotlin/org/jesperancinha/ktd/crums3/crum7/CrumSeven.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-3/src/main/kotlin/org/jesperancinha/ktd/crums3/crum7/CrumSeven.kt
@@ -4,6 +4,8 @@ import arrow.core.None
 import arrow.core.continuations.Effect
 import arrow.core.continuations.effect
 import arrow.core.continuations.ensureNotNull
+import arrow.core.left
+import arrow.core.right
 import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
 import java.io.File
 import java.io.FileNotFoundException
@@ -46,6 +48,7 @@ class CrumSeven {
         private val logger = object {
             fun info(logText: Any?) = ConsolerizerComposer.out().cyan(logText)
             fun info2(logText: Any?) = ConsolerizerComposer.out().blue(logText)
+            fun info3(logText: Any?) = ConsolerizerComposer.out().green(logText)
             fun infoTitle(logText: String) = ConsolerizerComposer.outSpace()
                 .yellow(ConsolerizerComposer.title(logText))
         }
@@ -55,11 +58,21 @@ class CrumSeven {
             logger.infoTitle("Crum 7 - Effects in Arrow from https://arrow-kt.io/docs/apidocs/arrow-core/arrow.core.continuations/-effect/")
             logger.info(readFile("").toEither())
             logger.info(readFile("knit.properties").toValidated())
+            logger.info(readFile("knit.properties").toValidated().left())
+            logger.info(readFile("knit.properties").toValidated().right())
+            logger.info3(readFile("memorycard1.txt").toValidated())
+            logger.info3(readFile("memorycard1.txt").toEither())
+            logger.info3(readFile("memorycard1.txt").toEither().left())
+            logger.info3(readFile("memorycard1.txt").toEither().right())
+            logger.info3(readFile("memorycard1.txt").toEither().getOrNull()?.body?.get(0))
             logger.info(readFile("gradle.properties").toIor())
             logger.info(readFile("README.MD").toOption { None })
             logger.info(readFile("build.gradle.kts").fold({ _: FileError -> null }, { it }))
             logger.info2(readFile2("").toEither())
             logger.info2(readFile2("knit.properties").toValidated())
+            logger.info2(readFile("memorycard2.txt").toEither())
+            logger.info2(readFile("memorycard2.txt").toEither().getOrNull()?.body?.get(0))
+            logger.info2(readFile("memorycard2.txt").toValidated())
             logger.info2(readFile2("gradle.properties").toIor())
             logger.info2(readFile2("README.MD").toOption { None })
             logger.info2(readFile2("build.gradle.kts").fold({ _: FileError -> null }, { it }))
