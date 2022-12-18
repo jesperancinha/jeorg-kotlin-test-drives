a55cf379ff3aafe78b4de420f6b8ebf4e67ca370
Joao Esperancinha <jofisaes@gmail.com>
2022-12-18 18:24:00 +0100

    Unit tests and coverage

---
+++ jeorg-kotlin-crums/jeorg-kotlin-optics-crums-1/src/test/java/org/jesperancinha/ktd/arrow/optics/crums1/LauncherArrowOpticsCrums1KtTest.kt
@@ -0,0 +1,12 @@
+package org.jesperancinha.ktd.arrow.optics.crums1
+
+import org.junit.jupiter.api.Test
+
+
+class LauncherArrowOpticsCrums1KtTest {
+
+    @Test
+    fun `should run all main crums`(){
+        main()
+    }
+}
