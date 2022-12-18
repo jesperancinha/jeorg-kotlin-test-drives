36ee89a3f1e8c9ec4f185f94a2ce0be4e5727dce
Joao Esperancinha <jofisaes@gmail.com>
2022-12-18 15:46:04 +0100

    Xmas effects

--- jeorg-kotlin-crums/jeorg-kotlin-optics-crums-1/src/main/kotlin/org/jesperancinha/ktd/arrow/optics/crums1/LauncherArrowOpticsCrums1.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-optics-crums-1/src/main/kotlin/org/jesperancinha/ktd/arrow/optics/crums1/LauncherArrowOpticsCrums1.kt
@@ -1,7 +1,9 @@
 package org.jesperancinha.ktd.arrow.optics.crums1
 
 import org.jesperancinha.ktd.arrow.optics.crums1.crum1.MailLauncher
+import org.jesperancinha.ktd.arrow.optics.crums1.crum2.XmasEffects
 
 fun main() {
     MailLauncher.main()
+    XmasEffects.main()
 }
--- jeorg-kotlin-crums/jeorg-kotlin-optics-crums-1/src/main/kotlin/org/jesperancinha/ktd/arrow/optics/crums1/crum2/XmasEffects.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-optics-crums-1/src/main/kotlin/org/jesperancinha/ktd/arrow/optics/crums1/crum2/XmasEffects.kt
@@ -1,5 +1,6 @@
 package org.jesperancinha.ktd.arrow.optics.crums1.crum2
 
+import arrow.core.left
 import arrow.optics.optics
 import org.jesperancinha.ktd.arrow.optics.crums1.crum2.Color.*
 import org.jesperancinha.ktd.arrow.optics.crums1.crum2.PendulumType.MIDDLE_AGE
@@ -37,11 +38,29 @@ data class Pendulum(
     val type: PendulumType = MIDDLE_AGE
 )
 
-data class Ball(
+@optics
+sealed class Ball(
     val radiusCm: Int = 4,
     val color: Color = GOLD,
     val pendulum: Pendulum = Pendulum()
-)
+) {
+    @optics
+    sealed class BigBall : Ball(radiusCm = 8) {
+        companion object
+    }
+
+    @optics
+    sealed class SilverBall : Ball(color = SILVER){
+        companion object
+    }
+
+    @optics
+    sealed class ExpensiveSilverBall : Ball(color = SILVER_METAL){
+        companion object
+    }
+
+    companion object
+}
 
 @optics
 sealed class Garland(
@@ -63,6 +82,40 @@ sealed class Garland(
 
     companion object
 }
+
+@optics
+sealed class XmasTree(
+    val balls: List<Ball> = emptyList(),
+    val garlands: List<Garland> = emptyList()
+) {
+    companion object
+}
+
+/**
+ * From top to bottom, a tree has 1 extra ball
+ * Because it's a 3D model, each level will have the double of balls of the previous one
+ * Single exception is the difference between the first and the second level where the first level always takes just one ball and the second 4
+ * If:
+ * 1. one floor does not have enough balls, then the tree gets unbalanced and we through InvalidTreeException
+ * 2. No tree should be made of expensive materials at this stage. An attempt to do so should throw ForbiddenMaterialsException
+ */
+fun createTree(balls: List<Ball>, garlands: List<Garland>) {
+    balls.first { ball: Ball -> ball.color.isExpensive() }.takeIf { it.color.isExpensive() }
+        ?: throw ForbiddenMaterialsException()
+    garlands.flatMap { it.colors }.first { color: Color -> color.isExpensive() }.takeIf { it.isExpensive() }
+        ?: throw ForbiddenMaterialsException()
+    val levels = when {
+        balls.size == 1 -> 1
+        (balls.size - 1) % 4 == 0 -> (balls.size - 1) / 4 + 1
+        else -> throw InvalidTreeException()
+    }
+
+
+
+}
+
+class ForbiddenMaterialsException : Exception()
+class InvalidTreeException : Exception()
 
 class XmasEffects {
     companion object {
