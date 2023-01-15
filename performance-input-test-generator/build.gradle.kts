80e1debbdc2c2dd633cbb69043b0a717899e722c
Joao Esperancinha <jofisaes@gmail.com>
2023-01-15 17:38:19 +0100

    Gradle to kts

---
+++ performance-input-test-generator/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    idea
+    java
+    jacoco
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+group "jesperancinha"
+version "0.0.0-SNAPSHOT"
+
+tasks.jar {
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map(::zipTree)
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.kohsuke.args4j:args4j-maven-plugin:2.33")
+    implementation("com.google.guava:guava:31.1-jre")
+    implementation("commons-io:commons-io:2.11.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.junit.platform:junit-platform-suite-engine:1.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
