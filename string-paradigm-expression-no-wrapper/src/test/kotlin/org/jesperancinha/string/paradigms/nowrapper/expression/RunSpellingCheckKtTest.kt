961febdd25ecc9e5489c325356216568a35321f0
Joao Esperancinha <jofisaes@gmail.com>
2023-01-17 22:22:49 +0100

    Adds unit tests

---
+++ string-paradigm-expression-no-wrapper/src/test/kotlin/org/jesperancinha/string/paradigms/nowrapper/expression/RunSpellingCheckKtTest.kt
@@ -0,0 +1,13 @@
+package org.jesperancinha.string.paradigms.nowrapper.expression
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class RunSpellingCheckKtTest {
+
+    @Test
+    fun `should run main`() {
+        main()
+    }
+}
