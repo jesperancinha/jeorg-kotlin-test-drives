9237810755413356b708a691f103745d3cac43d6
Joao Esperancinha <jofisaes@gmail.com>
2023-01-18 16:55:23 +0100

    Conversion complete

--- vertx-module/src/test/kotlin/org/jesperancinha/votd/FinanceVerticleTest.kt
+++ vertx-module/src/test/kotlin/org/jesperancinha/votd/FinanceVerticleTest.kt
@@ -1,46 +1,45 @@
 package org.jesperancinha.votd
 
-import io.vertx.core.AsyncResult
 import io.vertx.core.Vertx
-import io.vertx.core.http.HttpClientRequest
 import io.vertx.core.http.HttpMethod
 import io.vertx.junit5.VertxExtension
 import io.vertx.junit5.VertxTestContext
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import java.util.function.Function
 
 /**
  * Created by joao on 21-2-16.
  */
 @ExtendWith(VertxExtension::class)
-class FinanceVerticleTest {
-    private var vertx: Vertx? = null
-    @BeforeEach
-    fun setUp(context: VertxTestContext) {
-        vertx = Vertx.vertx()
-        vertx.deployVerticle(FinanceVerticle::class.java.name, context.succeedingThenComplete())
+class FinanceVerticleTest constructor(
+    context: VertxTestContext
+) {
+
+    init {
+        context.completeNow()
+    }
+    private val vertx: Vertx by lazy {
+        Vertx.vertx().also {
+            it.deployVerticle(FinanceVerticle::class.java.name, context.succeedingThenComplete())
+        }
     }
 
     @AfterEach
-    fun tearDown() {
-        vertx!!.close()
+    fun tearDown(context: VertxTestContext) {
+        vertx.close()
+        context.completeNow()
     }
 
     @Test
     fun testMyApplication(context: VertxTestContext) {
-        val async: Async = context.async()
-        vertx!!.createHttpClient().request(
+        vertx.createHttpClient().request(
             HttpMethod.GET, 8080, "localhost", "/"
-        ) { response: AsyncResult<HttpClientRequest?> ->
-            response.map(
-                Function<HttpClientRequest?, Any?> { body: HttpClientRequest? ->
+        ) { it.map {
 //                context.assertTrue(body.toString().contains("This is only the begining of cashing in"));
-                    async.complete()
-                    null
-                })
+                context.completeNow()
+                null
+            }
         }
     }
 }
