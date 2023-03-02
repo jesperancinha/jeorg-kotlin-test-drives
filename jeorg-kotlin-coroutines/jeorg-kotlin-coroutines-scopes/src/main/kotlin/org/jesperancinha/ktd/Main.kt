3fceeaba15b12b314b874ad9d29b89344dea55d4
Joao Esperancinha <jofisaes@gmail.com>
2023-03-02 11:31:20 +0100

    Git updates

---
+++ jeorg-kotlin-coroutines/jeorg-kotlin-coroutines-scopes/src/main/kotlin/org/jesperancinha/ktd/Main.kt
@@ -0,0 +1,10 @@
+package org.jesperancinha.ktd
+
+import kotlinx.coroutines.runBlocking
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) = runBlocking {
+        println("Hello world!")
+    }
+}
