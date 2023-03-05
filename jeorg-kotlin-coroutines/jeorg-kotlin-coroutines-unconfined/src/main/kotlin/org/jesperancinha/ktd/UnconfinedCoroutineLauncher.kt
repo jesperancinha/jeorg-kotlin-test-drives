0fb339b152773985cf8e108ae7aff62b33be0773
Joao Esperancinha <jofisaes@gmail.com>
2023-03-05 09:03:25 +0100

    The unconfined scope

--- jeorg-kotlin-coroutines/jeorg-kotlin-coroutines-unconfined/src/main/kotlin/org/jesperancinha/ktd/UnconfinedCoroutineLauncher.kt
+++ jeorg-kotlin-coroutines/jeorg-kotlin-coroutines-unconfined/src/main/kotlin/org/jesperancinha/ktd/UnconfinedCoroutineLauncher.kt
@@ -1,8 +1,48 @@
 package org.jesperancinha.ktd
 
+import kotlinx.coroutines.*
+import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
+import kotlin.math.log
+
 object UnconfinedCoroutineLauncher {
+    private val logger = object {
+        fun info(logText: Any?) = ConsolerizerComposer.out().yellow(logText)
+        fun infoBefore(logText: Any?) = ConsolerizerComposer.out().green(logText)
+        fun infoAfter(logText: Any?) = ConsolerizerComposer.out().red(logText)
+        fun infoTitle(logText: String) = ConsolerizerComposer.outSpace()
+            .cyan(ConsolerizerComposer.title(logText))
+
+        fun infoSubTitle(logText: String) = ConsolerizerComposer.outSpace()
+            .black()
+            .bgGreen(ConsolerizerComposer.title(logText))
+    }
+
     @JvmStatic
-    fun main(args: Array<String>) {
-        println("Hello world!")
+    fun main(args: Array<String>) = runBlocking {
+        logger.infoTitle(
+            "Coroutines scope tests / There are currently ${
+                Runtime.getRuntime().availableProcessors()
+            } CPU's available"
+        )
+        CoroutineScope(Dispatchers.Unconfined).launch {
+            launch {
+                logger.info("Running on context $coroutineContext")
+                logger.infoBefore("Siamese Cat is launching on Thread-${Thread.currentThread().threadId()}")
+                delay(1)
+                logger.infoAfter("Siamese Cat just ran on Thread-${Thread.currentThread().threadId()}")
+            }
+            launch {
+                logger.info("Running on context $coroutineContext")
+                logger.infoBefore("Mouse is launching on Thread-${Thread.currentThread().threadId()}")
+                delay(20)
+                logger.infoAfter("Mouse just ran on Thread-${Thread.currentThread().threadId()}")
+            }
+            launch {
+                logger.info("Running on context $coroutineContext")
+                logger.infoBefore("Maine Coon is launching on Thread-${Thread.currentThread().threadId()}")
+                delay(30)
+                logger.infoAfter("Maine Coon is just ran on Thread-${Thread.currentThread().threadId()}")
+            }
+        }.join()
     }
 }
--- jeorg-kotlin-coroutines/jeorg-kotlin-coroutines-unconfined/src/test/kotlin/org/jesperancinha/ktd/UnconfinedCoroutineLauncherTest.kt
+++ jeorg-kotlin-coroutines/jeorg-kotlin-coroutines-unconfined/src/test/kotlin/org/jesperancinha/ktd/UnconfinedCoroutineLauncherTest.kt
@@ -7,19 +7,6 @@ import org.junit.jupiter.api.Assertions.*
 
 class UnconfinedCoroutineLauncherTest {
 
-    private val logger = object {
-        fun info(logText: Any?) = ConsolerizerComposer.out().magenta(logText)
-        fun infoThread(logText: Any?) = ConsolerizerComposer.out().yellow(logText)
-        fun infoTs(logText: Any?) = ConsolerizerComposer.out().red(logText)
-        fun infoText(logText: Any?) = ConsolerizerComposer.out().green(logText)
-        fun infoTextSeparator(logText: Any?) = ConsolerizerComposer.out().orange(ConsolerizerComposer.title(logText))
-        fun infoTitle(logText: String) = ConsolerizerComposer.outSpace()
-            .cyan(ConsolerizerComposer.title(logText))
-        fun infoSubTitle(logText: String) = ConsolerizerComposer.outSpace()
-            .black()
-            .bgGreen(ConsolerizerComposer.title(logText))
-    }
-
     @Test
     fun main() {
     }
