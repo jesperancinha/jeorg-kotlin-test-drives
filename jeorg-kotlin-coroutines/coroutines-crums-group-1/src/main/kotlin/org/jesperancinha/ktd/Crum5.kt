9621131da394aef66f12658baa12e9d32e041138
Joao Esperancinha <jofisaes@gmail.com>
2023-03-30 15:35:35 +0200

    Cancellation exercises

--- jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/Crum5.kt
+++ jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/Crum5.kt
@@ -1,22 +1,53 @@
 package org.jesperancinha.ktd
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+
+object NeuronCancellationException : CancellationException()
+object NetworkCancellationException : Exception()
+
 
 class NeuralNetworksCancelling {
     companion object {
         @JvmStatic
-        fun main(args: Array<String> = emptyArray()): Unit = run {
+        fun main(args: Array<String> = emptyArray()): Unit = runBlocking {
             CoroutineScope(Dispatchers.Default).launch {
-                (1..100).forEach {
+                (1..10).forEach { rank ->
                     launch {
                         launch {
-                            launch { }
+                            launch {
+                                println("NeuronCancellationException - Only this one will be cancelled - Rank:$rank")
+                                throw NeuronCancellationException
+                            }
+                            delay(50)
+                            println("Prints Level 3 - Rank:$rank")
                         }
+                        delay(50)
+                        println("Prints Level 2 - Rank:$rank")
                     }
+                    delay(50)
+                    println("Prints Level 1 - Rank:$rank")
                 }
-            }
+            }.join()
+            CoroutineScope(Dispatchers.Default).launch {
+                (1..10).forEach { rank ->
+                    launch {
+                        launch {
+                            launch {
+                                println("NetworkCancellationException - Only this one will be cancelled - Rank:$rank")
+                                throw NetworkCancellationException
+                            }
+                            delay(50)
+                            println("Prints Level 3 - Rank:$rank")
+                        }
+                        delay(50)
+                        println("Prints Level 2 - Rank:$rank")
+                    }
+                    delay(50)
+                    println("Prints Level 1 - Rank:$rank")
+                }
+            }.join()
+            println("Finished CancellationExceptionTests")
+
         }
     }
 }
