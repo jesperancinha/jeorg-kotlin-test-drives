232cf7e7e16f54fb101c642b59c34262a99d9b8b
Joao Esperancinha <jofisaes@gmail.com>
2023-04-05 00:35:07 +0200

    First variance crum

---
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-4-variance/src/main/kotlin/org/jesperancinha/ktd/Main.kt
@@ -0,0 +1,62 @@
+package org.jesperancinha.ktd
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+interface Ingredients {
+    val list: List<String>
+}
+
+interface Part {
+    val name: String
+}
+
+open class Cake(
+    val ingredients: Ingredients
+){
+    var cakeName:String? = null
+        get() = if(ingredients.list.contains("flower") && ingredients.list.contains("egg")){
+                 "Pão de Ló"
+            }else {
+                null
+            }
+}
+
+class PaoDeLo(ingredients: Ingredients) : Cake(ingredients)
+
+@Suppress("UNCHECKED_CAST")
+class CakeShop<out T : Cake, in U : Ingredients, V : Part> {
+   fun makeCake(ingredients: U) = Cake(ingredients)
+
+// This would never work
+//    fun makeIngredients(cake: T) = object :Ingredients {
+//        override val list: List<String>
+//            get() = listOf("whatever")
+//
+//    } as U
+
+    fun repairDoor (door:V):V = logger.info("Door has been repaired!").let { door }
+
+    companion object {
+        val logger = LoggerFactory.getLogger(CakeShop::class.java)
+    }
+}
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val shop = CakeShop<PaoDeLo, Ingredients, Part>()
+        val cake = shop.makeCake(object : Ingredients {
+            override val list: List<String>
+                get() = listOf("flower", "egg")
+        })
+        logger.info("Cake ${cake.cakeName}has been made!")
+        shop.repairDoor(object: Part{
+            override val name: String
+                get() = "Sliding Door"
+
+        })
+    }
+
+    val logger:Logger = LoggerFactory.getLogger(Main::class.java)
+}
---
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-4-variance/src/test/kotlin/org/jesperancinha/ktd/MainTest.kt
@@ -0,0 +1,13 @@
+package org.jesperancinha.ktd
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainTest {
+
+    @Test
+    fun main() {
+        Main.main(emptyArray())
+    }
+}
