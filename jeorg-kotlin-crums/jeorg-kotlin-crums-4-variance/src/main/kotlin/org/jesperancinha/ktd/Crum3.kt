58577e2acdb86caec51e0d50a7cd6ca7363b92b6
Joao Esperancinha <jofisaes@gmail.com>
2023-04-08 19:27:30 +0200

    Make a clearer example

--- jeorg-kotlin-crums/jeorg-kotlin-crums-4-variance/src/main/kotlin/org/jesperancinha/ktd/Crum3.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-4-variance/src/main/kotlin/org/jesperancinha/ktd/Crum3.kt
@@ -3,18 +3,39 @@ package org.jesperancinha.ktd
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-interface ShellFood
+interface ShellFood {
+    fun exclusive()
+}
 
 open class Clams : ShellFood {
     override fun toString(): String = "Clams"
+
+    override fun exclusive() = logger.info("These clams are normal!")
+
+    companion object {
+        val logger:Logger= LoggerFactory.getLogger(Clams::class.java)
+    }
 }
 
 class Mussels : ShellFood {
+
     override fun toString(): String = "Mussels"
+
+    override fun exclusive() = logger.info("These mussels are big!")
+
+    companion object {
+        val logger:Logger= LoggerFactory.getLogger(Mussels::class.java)
+    }
 }
 
 class ClamsRoyal : Clams() {
     override fun toString(): String = "Clams Royal"
+
+    override fun exclusive() = logger.info("These clams are big!")
+
+    companion object {
+        val logger:Logger= LoggerFactory.getLogger(ClamsRoyal::class.java)
+    }
 }
 
 class Plate<out T : ShellFood>(
@@ -29,8 +50,13 @@ class Plate<out T : ShellFood>(
     }
 }
 
-class ContraPlate<in T : ShellFood> {
-    fun servePlate(food: T) = Plate.logger.info("Plate of $food has been served!")
+class ContraPlate<in T :ShellFood> {
+    fun servePlate(food: T) = logger.info("Plate of $food has been served!")
+        .also { food.exclusive() }
+
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(ContraPlate::class.java)
+    }
 }
 
 class ImmutableEatingOut {
@@ -46,8 +72,15 @@ class ImmutableEatingOut {
 
             val contraPlate1 = findPerfectContraPlate(0)
             contraPlate1.servePlate(ClamsRoyal())
+            (contraPlate1 as ContraPlate<Clams>).servePlate(Clams())
+            (contraPlate1 as ContraPlate<ShellFood>).servePlate(Mussels())
+            val contraPlate1A: ContraPlate<Clams> = contraPlate1
+            contraPlate1A.servePlate(Clams())
+
             val contraPlate2 = findPerfectContraPlate(1)
             contraPlate2.servePlate(ClamsRoyal())
+            (contraPlate2 as ContraPlate<Clams>).servePlate(Clams())
+
         }
 
         private fun findPerfectContraPlate(index: Int): ContraPlate<ClamsRoyal> = when (index) {
