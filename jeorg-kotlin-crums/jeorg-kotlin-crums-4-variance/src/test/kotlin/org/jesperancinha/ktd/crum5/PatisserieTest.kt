e27fb96e2df3ee733b372e748296e07ff1b21e5d
Joao Esperancinha <jofisaes@gmail.com>
2023-04-11 18:59:30 +0200

    Another example of contravariant types and its weird

--- jeorg-kotlin-crums/jeorg-kotlin-crums-4-variance/src/test/kotlin/org/jesperancinha/ktd/crum5/PatisserieTest.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-4-variance/src/test/kotlin/org/jesperancinha/ktd/crum5/PatisserieTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 
 class PatisserieTest {
     @Test
-    fun `should run cakes flawlessly`() {
+    fun `should run patisserie flawlessly`() {
         Patisserie.main()
     }
 }
