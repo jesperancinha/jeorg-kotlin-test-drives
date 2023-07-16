895d12fe1cce2e081edd9933c3c4b13525d2937a
Joao Esperancinha <jofisaes@gmail.com>
2023-07-16 21:02:14 +0200

    upgrades gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
-rootProject.name = "jeorg-kotlin-test-drives"
+rootProject.name = "jeorg-kotlin-test-drives-gradle"
 include("jeorg-kotlin-arrow-optics:jeorg-kotlin-arrow-optics-gradle-1")
 include("jeorg-kotlin-arrow-optics:jeorg-kotlin-optics-crums-1")
