4dce68681ec015e2865334eff854b12e78946fcf
Joao Esperancinha <jofisaes@gmail.com>
2023-11-09 22:54:13 +0100

    Adds delegation to maps example

--- jeorg-kotlin-crums/jeorg-kotlin-crums-4-variance/src/main/kotlin/org/jesperancinha/ktd/Delegates.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-4-variance/src/main/kotlin/org/jesperancinha/ktd/Delegates.kt
@@ -28,6 +28,14 @@ class Account(
     val number: String
 ) : Person by person, Card by card
 
+
+class User(details: Map<String, String>) {
+    val name by details
+    val address by details
+
+    override fun toString(): String = "User name is $name and address is $address"
+}
+
 class Delegates {
     companion object {
         @JvmStatic
@@ -48,6 +56,9 @@ class Delegates {
             println(account.address)
             println(account.cardNumber)
             println(account.expiryDate)
+
+            val user = User(mapOf("name" to "Priscilla", "address" to "Silicon Valley"))
+            println(user)
         }
     }
 }
