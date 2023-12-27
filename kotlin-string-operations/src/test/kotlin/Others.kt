fb991ad0129e4d1da3adcf3434997d0c97ee06f4
jesperancinha <jofisaes@gmail.com>
2023-12-27 14:51:12 +0100

    runningReduce tests

---
+++ kotlin-string-operations/src/test/kotlin/Others.kt
@@ -0,0 +1,27 @@
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Others {
+
+    @Test
+    fun `should running reduce correctly`() {
+        val testString = "That fish doesn't like strings"
+        val result = testString
+            .runningReduce { acc, c ->
+                c
+                    .takeIf { c > acc } ?: acc
+            }
+            .joinToString("")
+
+
+        val result2 = testString
+            .reduce { acc, c ->
+                c
+                    .takeIf { c > acc } ?: acc
+            }
+
+        result shouldBe "Thhttttttttttttttttttttttttttt"
+        result.length shouldBe testString.length
+        result2 shouldBe 't'
+    }
+}
