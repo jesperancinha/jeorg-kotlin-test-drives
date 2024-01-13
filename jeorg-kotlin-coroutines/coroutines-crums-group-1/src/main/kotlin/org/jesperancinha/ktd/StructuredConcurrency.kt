d04759e2bbc5c908e159e9218ff280c557295627
jesperancinha <jofisaes@gmail.com>
2024-01-13 19:57:41 +0100

    Examples with cancellation

--- jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/StructuredConcurrency.kt
+++ jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/StructuredConcurrency.kt
@@ -1,6 +1,10 @@
 package org.jesperancinha.ktd
 
 import kotlinx.coroutines.*
+import org.jesperancinha.ktd.CancellationStructuredConcurrency.Companion.testCancel2LevelBlockingLaunchBlockEvenLoop
+import org.jesperancinha.ktd.CancellationStructuredConcurrency.Companion.testCancelBlockingLaunchBlockEvenLoop
+import org.jesperancinha.ktd.StandardStructuredConcurrency.Companion.testBLockingLaunchBlockEvenLoop
+import org.jesperancinha.ktd.StandardStructuredConcurrency.Companion.testBlockingLaunchDispatchersIO
 import java.time.LocalDateTime
 import kotlin.system.measureTimeMillis
 
@@ -11,12 +15,107 @@ class StructuredConcurrency {
         fun main(args: Array<String> = emptyArray()) {
             testBLockingLaunchBlockEvenLoop()
             testBlockingLaunchDispatchersIO()
+            testCancelBlockingLaunchBlockEvenLoop()
+            testCancel2LevelBlockingLaunchBlockEvenLoop()
+        }
+
+    }
+
+}
+
+class CancellationStructuredConcurrency {
+    companion object {
+        fun testCancelBlockingLaunchBlockEvenLoop() = runBlocking {
+            measureTimeMillis {
+                kotlin.runCatching {
+                    coroutineScope {
+
+                        launch {
+                            println("I'm coroutine 1 on thread ${Thread.currentThread()}")
+                            delay(1000)
+                            println(
+                                "I'm coroutine 1 on thread ${
+                                    Thread.currentThread()
+                                } and I am finishing on ${LocalDateTime.now()}"
+                            )
+                        }
+
+                        launch {
+                            println("I'm coroutine 2 on thread ${Thread.currentThread()}")
+                            delay(1000)
+                            println(
+                                "I'm coroutine 2 on thread ${
+                                    Thread.currentThread()
+                                } and I am finishing on ${LocalDateTime.now()}"
+                            )
+                        }
+
+                        launch {
+                            delay(500)
+                            throw RuntimeException("Nothing runs now!")
+                        }
+                        println("This coroutine scope is launched here! ${this.coroutineContext}")
+                    }
+
+                }
+            }.let { println("As expected, this run took only $it milliseconds to complete.") }
+
+            println("When the coroutineScope is finished, I can finally finish the program")
         }
 
 
+        fun testCancel2LevelBlockingLaunchBlockEvenLoop() = runBlocking {
+            measureTimeMillis {
+                kotlin.runCatching {
+                    coroutineScope {
+
+                        launch {
+                            println("I'm coroutine 1 on thread ${Thread.currentThread()}")
+                            delay(1000)
+                            println(
+                                "I'm coroutine 1 on thread ${
+                                    Thread.currentThread()
+                                } and I am finishing on ${LocalDateTime.now()}"
+                            )
+                        }
+
+                        launch {
+                            println("I'm coroutine 2 on thread ${Thread.currentThread()}")
+                            launch {
+                                delay(500)
+                                throw RuntimeException("Nothing runs now!")
+                            }
+                            delay(1000)
+                            println(
+                                "I'm coroutine 2 on thread ${
+                                    Thread.currentThread()
+                                } and I am finishing on ${LocalDateTime.now()}"
+                            )
+                        }
+
+
+                        println("This coroutine scope is launched here! ${this.coroutineContext}")
+                    }
+
+                }
+            }.let { println("As expected, this run took only $it milliseconds to complete.") }
+
+            println("When the coroutineScope is finished, I can finally finish the program")
+        }
+
+
+    }
+
+}
+
+class StandardStructuredConcurrency {
+
+    companion object {
         fun testBLockingLaunchBlockEvenLoop() = runBlocking {
             measureTimeMillis {
+
                 coroutineScope {
+
                     launch {
                         println("I'm coroutine 1 on thread ${Thread.currentThread()}")
                         delay(1000)
@@ -39,6 +138,7 @@ class StructuredConcurrency {
 
                     println("This coroutine scope is launched here! ${this.coroutineContext}")
                 }
+
             }.let { println("As expected, this run took only $it milliseconds to complete.") }
 
             println("When the coroutineScope is finished, I can finally finish the program")
@@ -89,7 +189,5 @@ class StructuredConcurrency {
             println("When the coroutineScope is finished, I can finally finish the program")
         }
 
-
     }
-
 }
