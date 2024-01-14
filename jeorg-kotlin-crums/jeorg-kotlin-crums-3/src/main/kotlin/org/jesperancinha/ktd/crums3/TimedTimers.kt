dd68e3057056306ceff02b919035d5878c4fae80
jesperancinha <jofisaes@gmail.com>
2024-01-14 15:55:57 +0100

    Testing timed TImers

--- jeorg-kotlin-crums/jeorg-kotlin-crums-3/src/main/kotlin/org/jesperancinha/ktd/crums3/TimedTimers.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-3/src/main/kotlin/org/jesperancinha/ktd/crums3/TimedTimers.kt
@@ -1,11 +1,14 @@
 package org.jesperancinha.ktd.crums3
 
+import arrow.atomic.AtomicInt
 import java.lang.Thread.sleep
 import java.time.LocalDateTime
 import java.util.*
 
 
 class SleepingTimerTask : TimerTask() {
+
+    val counter = AtomicInt(0)
 
     /**
      * Explicitly blocking to simulate task
@@ -14,7 +17,13 @@ class SleepingTimerTask : TimerTask() {
     override fun run() {
         println("Task started at ${LocalDateTime.now()}")
         sleep(1000)
+        counter.addAndGet(1)
+        callLongProcess()
         println("Task ended at ${LocalDateTime.now()}")
+    }
+
+    private fun callLongProcess() {
+        sleep(5000)
     }
 
     companion object {
@@ -24,13 +33,15 @@ class SleepingTimerTask : TimerTask() {
          */
         @JvmStatic
         fun main(args: Array<String>) {
-            val timerTask: TimerTask = SleepingTimerTask()
+            val timerTask = SleepingTimerTask()
             val timer = Timer(true)
             timer.scheduleAtFixedRate(timerTask, 0, 2_000)
             println("TimerTask has started and now we wait 10 seconds")
+            println("Normally it should count 5 calls, but maybe not now, because of the long call")
             sleep(10_000)
             timer.cancel()
             println("TimerTask has been cancelled")
+            println("${timerTask.counter} calls have been made!")
         }
     }
 }
