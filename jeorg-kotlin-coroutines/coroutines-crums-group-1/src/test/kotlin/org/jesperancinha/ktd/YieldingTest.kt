967c4076ae7968c64e51671702b1df9f35670077
jesperancinha <jofisaes@gmail.com>
2024-01-15 14:10:02 +0100

    Testing runTest

---
+++ jeorg-kotlin-coroutines/coroutines-crums-group-1/src/test/kotlin/org/jesperancinha/ktd/YieldingTest.kt
@@ -0,0 +1,55 @@
+package org.jesperancinha.ktd
+
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.comparables.shouldBeGreaterThan
+import io.kotest.matchers.nulls.shouldNotBeNull
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.measureTime
+
+class YieldingTest {
+    @Test
+    fun `should timeout after 10 seconds`() {
+        measureTime {
+            runTest {
+                kotlin.runCatching {
+                    println(backgroundScope)
+                    val job = backgroundScope.launch {
+                        while (isActive) {
+                            yield()
+                        }
+                    }
+                    delay(1)
+                    job.shouldNotBeNull()
+                }
+            }
+        }.shouldBeGreaterThan(10.seconds)
+    }
+
+    @Test
+    fun `should cancel with runBlocking`(): Unit = runBlocking {
+        println(this)
+        val job = this.launch {
+            while (isActive) {
+                yield()
+            }
+        }
+        delay(1)
+        job.cancelAndJoin()
+        job.isCancelled.shouldBeTrue()
+    }
+
+    @Test
+    fun `should cancel with runTest`() = runTest {
+        val job = backgroundScope.launch {
+            while (isActive) {
+                delay(1)
+            }
+        }
+        delay(10)
+        job.cancelAndJoin()
+        job.isCancelled.shouldBeTrue()
+    }
+}
