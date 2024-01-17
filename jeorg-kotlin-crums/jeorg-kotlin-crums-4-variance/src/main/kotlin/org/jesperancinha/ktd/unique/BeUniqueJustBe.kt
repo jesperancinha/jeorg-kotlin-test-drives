d9fc2a5960d347a8b440dc6d0cfc951d52390a00
jesperancinha <jofisaes@gmail.com>
2024-01-17 09:20:34 +0100

    The great text KDoc test

--- jeorg-kotlin-crums/jeorg-kotlin-crums-4-variance/src/main/kotlin/org/jesperancinha/ktd/unique/BeUniqueJustBe.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-4-variance/src/main/kotlin/org/jesperancinha/ktd/unique/BeUniqueJustBe.kt
@@ -6,4 +6,25 @@ package org.jesperancinha.ktd.unique
     fun main() {
         val books = listOf(Book(1), Book(2), Book(4), Book(1))
         println(books.distinct())
+
+        GreatClass(1)
+        GreatClass("1")
     }
+
+
+/**
+ * Great class to store values
+ *
+ * @property text A String
+ * @constructor Store a string.
+ * If you want to use an integer please use the secondary constructor with an [Int] as input argument
+ */
+class GreatClass(private val text: String) {
+
+    /**
+     * This is the other constructor
+     * @param aNumber Integer
+     */
+    constructor(aNumber: Int) : this("You gave me a $aNumber")
+
+}
