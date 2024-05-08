901063143853abd78abdbb4312d7b87a5502b088
jesperancinha <jofisaes@gmail.com>
2024-05-08 10:55:02 +0200

    Refactors Makefile and adds TOML to dependency definitions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 rootProject.name = "jeorg-kotlin-test-drives-gradle"
+include("jeorg-kotlin-arrow-optics")
 include("jeorg-kotlin-arrow-optics:jeorg-kotlin-arrow-optics-gradle-1")
 include("jeorg-kotlin-arrow-optics:jeorg-kotlin-optics-crums-1")
 include("experiments")
