5bb7c91e34b2f5ad75444afa23085e6b818704ab
jesperancinha <jofisaes@gmail.com>
2024-06-14 08:21:20 +0200

    lens for us

--- jeorg-kotlin-arrow-optics/jeorg-kotlin-arrow-optics-gradle-1/src/main/kotlin/org/jesperancinha/ktd/crums3/crum1/CrumOne.kt
+++ jeorg-kotlin-arrow-optics/jeorg-kotlin-arrow-optics-gradle-1/src/main/kotlin/org/jesperancinha/ktd/crums3/crum1/CrumOne.kt
@@ -8,8 +8,9 @@ import org.jesperancinha.console.consolerizer.console.ConsolerizerComposer
 data class Account(val balance: Int, val available: Int) {
     companion object
 }
+
 class CrumOne {
-    companion object{
+    companion object {
         private val logger = object {
             fun info(logText: Any) = ConsolerizerComposer.out().red(logText)
             fun infoTitle(logText: String) = ConsolerizerComposer.outSpace()
@@ -21,10 +22,13 @@ class CrumOne {
             logger.infoTitle("Crum 1 - Optics in Arrow with code generation")
             logger.info("Hello World!")
             logger.info("Program arguments: ${args.joinToString()}")
-
+            val account = Account(1000, 2000)
             val balanceLens: Lens<Account, Int> = Account.balance
+            val availableLens: Lens<Account, Int> = Account.available
 
             println(balanceLens)
+            println(balanceLens.get(account))
+            println(availableLens.get(account))
         }
     }
 }
