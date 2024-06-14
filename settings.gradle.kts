7e55721d545f8d494f59246f73c93d0e6af2a6f3
jesperancinha <jofisaes@gmail.com>
2024-06-14 08:27:45 +0200

    renames optics module to correct name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "jeorg-kotlin-test-drives-gradle"
 include("jeorg-kotlin-arrow-optics")
-include("jeorg-kotlin-arrow-optics:jeorg-kotlin-arrow-optics-gradle-1")
+include("jeorg-kotlin-arrow-optics:jeorg-kotlin-optics-crums-2")
 include("jeorg-kotlin-arrow-optics:jeorg-kotlin-optics-crums-1")
 include("experiments")
 include("experiments:sealed-interfaces-interops")
