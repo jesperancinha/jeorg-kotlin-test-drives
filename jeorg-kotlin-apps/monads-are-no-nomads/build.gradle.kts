dd0704bcaaae40d921bbc5cba7cd89dce6e8515c
jesperancinha <jofisaes@gmail.com>
2024-07-16 11:34:42 +0200

    Starts monad project

---
+++ jeorg-kotlin-apps/monads-are-no-nomads/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.kotlin.jvm)
+    application
+    idea
+    jacoco
+    alias(libs.plugins.jesperancinha.omni)
+}
+
+group = "org.jesperancinha"
+version = "0.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    dependencies {
+        implementation(platform(libs.arrow.stack))
+        implementation("io.arrow-kt:arrow-core")
+        implementation("io.arrow-kt:arrow-optics")
+        implementation("io.arrow-kt:arrow-fx-coroutines")
+        implementation(libs.arrow.analysis.types.jvm)
+        implementation(libs.consolerizer)
+        implementation(libs.kotest.assertions.core.jvm)
+        ksp(libs.arrow.optics.ksp.plugin)
+        testImplementation (libs.junit.jupiter.api)
+        testImplementation (libs.junit.jupiter.engine)
+        testImplementation(libs.kotlin.test)
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
