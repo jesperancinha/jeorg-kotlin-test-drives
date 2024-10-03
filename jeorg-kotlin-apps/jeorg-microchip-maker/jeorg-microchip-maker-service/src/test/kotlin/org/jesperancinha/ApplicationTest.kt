04c6cee2e1bd6bfd30f4ec1ede41e5f6a61eead8
jesperancinha <jofisaes@gmail.com>
2024-10-03 11:50:09 +0200

    Upgrades test

--- jeorg-kotlin-apps/jeorg-microchip-maker/jeorg-microchip-maker-service/src/test/kotlin/org/jesperancinha/ApplicationTest.kt
+++ jeorg-kotlin-apps/jeorg-microchip-maker/jeorg-microchip-maker-service/src/test/kotlin/org/jesperancinha/ApplicationTest.kt
@@ -1,27 +1,14 @@
 package org.jesperancinha
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.server.auth.*
-import io.ktor.util.*
-import io.ktor.server.auth.jwt.*
-import com.auth0.jwt.JWT
-import com.auth0.jwt.JWTVerifier
-import com.auth0.jwt.algorithms.Algorithm
-import io.ktor.serialization.jackson.*
-import com.fasterxml.jackson.databind.*
-import io.ktor.server.plugins.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import org.jesperancinha.plugins.*
+import org.jesperancinha.plugins.configureRouting
 import java.time.LocalDateTime
 import java.time.ZoneId
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
