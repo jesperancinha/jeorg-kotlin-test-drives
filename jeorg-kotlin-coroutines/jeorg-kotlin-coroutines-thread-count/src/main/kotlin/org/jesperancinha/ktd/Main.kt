a3998a3417a371f96b8d4134712dadf4d9c55706
jesperancinha <jofisaes@gmail.com>
2024-11-05 09:23:12 +0100

    Supervisor scope example

--- jeorg-kotlin-coroutines/jeorg-kotlin-coroutines-thread-count/src/main/kotlin/org/jesperancinha/ktd/Main.kt
+++ jeorg-kotlin-coroutines/jeorg-kotlin-coroutines-thread-count/src/main/kotlin/org/jesperancinha/ktd/Main.kt
@@ -1,8 +1,26 @@
 package org.jesperancinha.ktd
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
+
 object Main {
     @JvmStatic
-    fun main(args: Array<String>) {
+    fun main(args: Array<String>) = runBlocking {
         println("Hello world!")
+        supervisorScope {
+            val job1 = launch {
+                delay(1000)
+                throw RuntimeException("Failure in job 1")
+            }
+
+            val job2 = launch {
+                delay(2000)
+                println("Job 2 completed successfully")
+            }
+            joinAll(job1, job2)
+        }
     }
 }
