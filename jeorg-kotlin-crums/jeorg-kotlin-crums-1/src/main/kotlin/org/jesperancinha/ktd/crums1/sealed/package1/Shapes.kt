5ea9c86ece48974a766d6df450d4b96c3b04ca86
João Esperancinha <jofisaes@gmail.com>
2024-12-08 09:26:24 +0100

    Sealed

--- jeorg-kotlin-crums/jeorg-kotlin-crums-1/src/main/kotlin/org/jesperancinha/ktd/crums1/sealed/package1/Shapes.kt
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-1/src/main/kotlin/org/jesperancinha/ktd/crums1/sealed/package1/Shapes.kt
@@ -1,9 +1,4 @@
 package org.jesperancinha.ktd.crums1.sealed.package1
 
 sealed class Shape
-
-data class Circle(val radius: Double) : Shape()
-data class Rectangle(val width: Double, val height: Double) : Shape()
-
-
 sealed class AnotherShape
---
+++ jeorg-kotlin-crums/jeorg-kotlin-crums-1/src/main/kotlin/org/jesperancinha/ktd/crums1/sealed/package1/ShapesExtended.kt
@@ -0,0 +1,4 @@
+package org.jesperancinha.ktd.crums1.sealed.package1
+
+data class Circle(val radius: Double) : Shape()
+data class Rectangle(val width: Double, val height: Double) : Shape()
