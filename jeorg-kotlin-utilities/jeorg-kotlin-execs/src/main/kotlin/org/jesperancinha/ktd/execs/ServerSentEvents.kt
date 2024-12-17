c5ec9ac9f5d4ddc28eb308eadd7ecac719df0dc8
jesperancinha <jofisaes@gmail.com>
2024-12-17 13:00:19 +0100

    improves reading and understanding the nature of repeat

--- jeorg-kotlin-utilities/jeorg-kotlin-execs/src/main/kotlin/org/jesperancinha/ktd/execs/ServerSentEvents.kt
+++ jeorg-kotlin-utilities/jeorg-kotlin-execs/src/main/kotlin/org/jesperancinha/ktd/execs/ServerSentEvents.kt
@@ -3,24 +3,36 @@ package org.jesperancinha.ktd.execs
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
+import java.util.concurrent.atomic.AtomicInteger
 
-const val sentence = "In the garden of Eden"
+private const val sentence = "Who is awesome? You are!"
+
+
+val counter = AtomicInteger(0)
 
 /**
  * SSE
  */
 fun randomWordFlow(): Flow<String> = flow {
     val words = sentence.split(" ")
-    repeat(1000) {
+    repeat(5) {
         emit(words.random())
     }
+    counter.incrementAndGet()
 }
 
 fun callFlow() = runBlocking {
-    randomWordFlow().collect { word -> print(" $word") }
+    val randomWordFlow = randomWordFlow()
+    randomWordFlow.collect { word -> print(word) }
+    println(counter.get())
+    return@runBlocking randomWordFlow
 }
 
-fun main() {
-    callFlow()
+fun main(): Unit = runBlocking {
+    val callFlow = callFlow()
+    repeat(5) {
+        callFlow.collect { word -> print(word) }
+        println(counter.get())
+    }
 }
 
