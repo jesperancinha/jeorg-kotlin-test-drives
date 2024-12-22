e41c77073b671b6539d1ac1882823a332874bb8b
jesperancinha <jofisaes@gmail.com>
2024-12-22 11:29:25 +0100

    Channels update

--- jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/TryEmitTrySend.kt
+++ jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/TryEmitTrySend.kt
@@ -1,30 +1,48 @@
 package org.jesperancinha.ktd
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 class TryEmitTrySend {
     companion object {
         @JvmStatic
         fun main(args: Array<String> = emptyArray()): Unit = runBlocking {
-            val sharedFlow = MutableSharedFlow<String>()
+            runCatching {
+                withTimeout(5.seconds) {
 
-            launch {
-                val messages = listOf("Event 1", "Event 2", "Event 3")
-                for (message in messages) {
-                    println("Emitting: $message")
-                    sharedFlow.tryEmit(message)
-                    delay(500)
-                }
-            }
+                    val scope = CoroutineScope(Dispatchers.Default)
 
-            repeat(2) { collectorId ->
-                launch {
-                    sharedFlow.collect { event ->
-                        println("Collector $collectorId received: $event")
-                    }
+                    scope.launch {
+                        val sharedFlow = MutableSharedFlow<String>(3)
+
+                        val jobEmitter = launch {
+                            val messages = listOf("Event 1", "Event 2", "Event 3")
+                            for (message in messages) {
+                                println("Emitting: $message")
+                                sharedFlow.tryEmit(message)
+                                delay(500)
+                            }
+                        }
+                        jobEmitter.join()
+                        jobEmitter.cancel()
+
+                        val job = launch {
+                            sharedFlow.collect { event ->
+                                println("Collector received: $event")
+                            }
+                        }
+                        job.join()
+                        job.cancel()
+                    }.join()
+                    scope.cancel()
                 }
             }
         }
