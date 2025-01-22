4fd78a0a5de574784040a22ac213f8c485e4999e
João Esperancinha <jofisaes@gmail.com>
2025-01-22 09:32:12 +0100

    Java and Kotlin

--- jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/BroadcastChannel.kt
+++ jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/BroadcastChannel.kt
@@ -4,27 +4,31 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import kotlin.time.Duration.Companion.seconds
 
 class BroadcastChannel {
     companion object {
         @JvmStatic
         fun main(args: Array<String> = emptyArray()): Unit = runBlocking {
-            val sharedFlow = MutableSharedFlow<Int>(replay = 0)
-            launch {
-                sharedFlow.collect { value ->
-                    println("Subscriber 1 received: $value")
+            withTimeout(10.seconds) {
+                val sharedFlow = MutableSharedFlow<Int>(replay = 0)
+                launch {
+                    sharedFlow.collect { value ->
+                        println("Subscriber 1 received: $value")
+                    }
                 }
-            }
 
-            launch {
-                sharedFlow.collect { value ->
-                    println("Subscriber 2 received: $value")
+                launch {
+                    sharedFlow.collect { value ->
+                        println("Subscriber 2 received: $value")
+                    }
                 }
-            }
 
-            repeat(5) {
-                sharedFlow.emit(it)
-                delay(100)
+                repeat(5) {
+                    sharedFlow.emit(it)
+                    delay(100)
+                }
             }
         }
     }
