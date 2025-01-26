5032fdd4c60dfca1545e3000c2105e5c9e4542ef
João Esperancinha <jofisaes@gmail.com>
2025-01-26 19:29:33 +0100

    Default dispatchers

--- jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/CoroutineJob.kt
+++ jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/CoroutineJob.kt
@@ -1,9 +1,6 @@
 package org.jesperancinha.ktd
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import java.lang.Thread.sleep
+import kotlinx.coroutines.*
 import kotlin.system.measureTimeMillis
 
 class CoroutineJob {
@@ -13,17 +10,18 @@ class CoroutineJob {
             println(
                 "Execution time took ${
                     measureTimeMillis {
-                        runBlocking {
+                        runBlocking(Dispatchers.Default) {
                             launch {
                                 println("Starting coroutine job 1")
                                 delay(2000)
-                                println("Finished coroutine job 1")
+                                println("Finished coroutine job 1 with context ${currentCoroutineContext()} on Thread ${Thread.currentThread()}")
                             }
                             launch {
                                 println("Started coroutine job 2")
                                 delay(4000)
-                                println("Finished coroutine job 2")
+                                println("Finished coroutine job 2 with context ${currentCoroutineContext()} on Thread ${Thread.currentThread()}")
                             }
+                            println("Finished runBlocking coroutine with context ${currentCoroutineContext()} on Thread ${Thread.currentThread()}")
                         }
                     }
                 }ms")
