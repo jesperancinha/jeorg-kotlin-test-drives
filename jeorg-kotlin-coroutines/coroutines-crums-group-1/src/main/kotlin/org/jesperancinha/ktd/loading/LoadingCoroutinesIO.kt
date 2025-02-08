170fcac4935416c1593317f09dfb6c103773b842
João Esperancinha <jofisaes@gmail.com>
2025-02-08 11:32:29 +0100

    Dispatchers.IO

--- jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/loading/LoadingCoroutinesIO.kt
+++ jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/loading/LoadingCoroutinesIO.kt
@@ -1,23 +1,40 @@
 package org.jesperancinha.ktd.loading
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import java.io.File
+import java.lang.Thread.sleep
+import kotlin.system.measureTimeMillis
 
 object LoadingCoroutinesIO {
+    val file = File("example.txt").apply { writeText("Kotlin Coroutines Dispatchers.IO") }
+    
     @JvmStatic
     fun main(args: Array<String>) = runBlocking {
-        val file = File("example.txt").apply { writeText("Kotlin Coroutines Dispatchers.IO") }
+        runTest(10)
+        runTest(50)
+        runTest(100)
+        runTest(200)
+        runTest(500)
+        runTest(1000)
+        runTest(5000)
+        runTest(10000)
+    }
 
-        repeat(100) { index ->
-            launch(Dispatchers.IO) {
-                val threadName = Thread.currentThread().name
-                val content = file.readText()
-                println("[$index] Read by thread: $threadName -> ${content.take(20)}...")
-                delay(100)
-            }
-        }
+    private suspend fun CoroutineScope.runTest(max: Int) {
+        println("-------------------------")
+        println("Launching $max coroutines")
+        measureTimeMillis {
+            (1..max).map { index ->
+                launch(Dispatchers.IO) {
+                    file.readText()
+                    sleep(10)
+                }
+            }.joinAll()
+            println(
+                "${
+                    Thread.currentThread().threadGroup.activeCount()
+                } threads have been traced."
+            )
+        }.run { println("Took ${this} ms to complete!") }
     }
 }
