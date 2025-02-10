0c9b67db31844e08f587267ad9c8f6301dcbc747
jesperancinha <jofisaes@gmail.com>
2025-02-10 22:05:16 +0100

    update example

--- jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/loading/LoadingCoroutinesUnconfined.kt
+++ jeorg-kotlin-coroutines/coroutines-crums-group-1/src/main/kotlin/org/jesperancinha/ktd/loading/LoadingCoroutinesUnconfined.kt
@@ -1,6 +1,7 @@
 package org.jesperancinha.ktd.loading
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -10,9 +11,19 @@ object LoadingCoroutinesUnconfined {
     @JvmStatic
     fun main(args: Array<String>): Unit = runBlocking {
         launch(Dispatchers.Unconfined) {
-            println("Start in thread: ${Thread.currentThread().name}")
+            println("1 - Start in thread: ${Thread.currentThread().name} with coroutine context ${currentCoroutineContext()}")
             delay(1000)
-            println("Resumed in thread: ${Thread.currentThread().name}")
+            println("1 - Resumed in thread: ${Thread.currentThread().name} with coroutine context ${currentCoroutineContext()}")
+            delay(1000)
+            println("1 - Resumed in thread: ${Thread.currentThread().name} with coroutine context ${currentCoroutineContext()}")
+        }
+
+        launch(Dispatchers.Unconfined) {
+            println("2 - Start in thread: ${Thread.currentThread().name} with coroutine context ${currentCoroutineContext()}")
+            delay(1000)
+            println("2 - Resumed in thread: ${Thread.currentThread().name} with coroutine context ${currentCoroutineContext()}")
+            delay(1000)
+            println("2 - Resumed in thread: ${Thread.currentThread().name} with coroutine context ${currentCoroutineContext()}")
         }
     }
 }
