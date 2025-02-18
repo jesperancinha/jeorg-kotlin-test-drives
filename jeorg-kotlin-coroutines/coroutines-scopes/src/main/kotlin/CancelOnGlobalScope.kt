ad3a3047566bb6499dee5a0ab14ea15962fbdad7
João Esperancinha <jofisaes@gmail.com>
2025-02-18 18:34:35 +0100

    Fresh start

---
+++ jeorg-kotlin-coroutines/coroutines-scopes/src/main/kotlin/CancelOnGlobalScope.kt
@@ -0,0 +1,60 @@
+import kotlinx.coroutines.*
+import java.lang.Thread.*
+
+object CancelOnGlobalScope {
+    @JvmStatic
+    fun main(args: Array<String> = emptyArray()) {
+        println("The context of the global scope is ${GlobalScope.coroutineContext}")
+        GlobalScope.launch {
+            coroutineScope {
+                launch {
+                    println("Current Thread is ${currentThread()}, and context is ${coroutineContext}")
+                    delay(1000)
+                    println("This will be cancelled")
+                }
+                println("Current Thread is ${currentThread()}, and context is ${coroutineContext}")
+                throw RuntimeException("Cancelling everything?")
+            }
+            println("Current Parent Thread is ${currentThread()}, and context is ${coroutineContext}")
+
+        }
+        println("Current Parent Thread is ${currentThread()}")
+        sleep(2000)
+        println("Still running")
+    }
+}
+
+object CancleOnRunBlocking {
+    @JvmStatic
+    fun main(args: Array<String> = emptyArray())  {
+        runBlocking(Dispatchers.IO) {
+            println("The context of the global scope is ${GlobalScope.coroutineContext}")
+            coroutineScope {
+                launch {
+                    println("Current Thread is ${currentThread()}, and context is ${coroutineContext}")
+                    delay(1000)
+                    println("This will be cancelled")
+                }
+                println("Current Thread is ${currentThread()}, and context is ${coroutineContext}")
+                throw RuntimeException("Cancelling everything")
+            }
+            println("Current Parent Thread is ${currentThread()}, and context is ${coroutineContext}")
+
+        }
+        println("Current Parent Thread is ${currentThread()}")
+        sleep(2000)
+        println("Still running")
+    }
+}
+
+object CancelOnExample {
+    @JvmStatic
+    fun main(args: Array<String> = emptyArray()) {
+        CancelOnGlobalScope.main()
+        runCatching {
+            CancleOnRunBlocking.main()
+        }.onFailure {
+            it.printStackTrace()
+        }
+    }
+}
--- jeorg-kotlin-coroutines/coroutines-scopes/src/main/kotlin/Main.kt
+++ jeorg-kotlin-coroutines/coroutines-scopes/src/main/kotlin/Main.kt
@@ -1,2 +1,3 @@
 fun main() {
+    CancelOnExample.main()
 }
