175f041049aa483a8c1fd8003d3c566abf724144
João Esperancinha <jofisaes@gmail.com>
2025-05-15 09:46:57 +0200

    Corrects Kotlin JVM version

--- jeorg-kotlin-apps/performance-projects/performance-input-test-generator/build.gradle.kts
+++ jeorg-kotlin-apps/performance-projects/performance-input-test-generator/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.1.21"
+    alias(libs.plugins.kotlin.jvm)
     idea
     java
     jacoco
--- jeorg-kotlin-crums/module-use/build.gradle.kts
+++ jeorg-kotlin-crums/module-use/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.1.21"
+    alias(libs.plugins.kotlin.jvm)
 }
 
 group = "org.jesperancinha.asnsei.modularity"
